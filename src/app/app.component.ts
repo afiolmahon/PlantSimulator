4c6af33a702e4c333bca2cd9b6a84255e38acd22
Antonio Fiol-Mahon <afiolmahon@gmail.com>
2019-04-03 15:28:04 -0400

    demo scene created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('canvas') private canvasRef: ElementRef;
+
+  private renderer: THREE.WebGLRenderer;
+  private scene: THREE.Scene;
+  
+  private testCube: THREE.Mesh;
+  private t: number = 0; // animation timer/
+
+  private camera : THREE.PerspectiveCamera;
   title = 'PlantSimulator';
+
+  constructor() {
+    this.testCube = new THREE.Mesh(new THREE.BoxGeometry(2,2,2), new THREE.MeshBasicMaterial({ color: 0x457a45 }));
+  }
+
+  ngOnInit() {
+    this.createScene();
+    this.startRenderingLoop();
+  }
+
+  getAspectRatio(): number { return window.innerWidth / window.innerHeight; }
+
+  private createScene() {
+    this.camera = new THREE.PerspectiveCamera(40, this.getAspectRatio(), 1, 1000);
+    this.camera.position.setY(-10);
+    this.camera.position.setZ(10);
+    this.camera.rotateX(Math.PI/4)
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x54a1ff);
+    // Add geometry to scene
+    this.scene.add(this.testCube);
+  }
+
+  private get canvas(): HTMLCanvasElement { return this.canvasRef.nativeElement; }
+
+  // onWindowResize(_: Event) {
+//   this.cameraControl.camera.aspect = this.getAspectRatio();
+//   this.renderer.setSize(window.innerWidth, window.innerHeight);
+//   this.cameraControl.updateCamera();
+// }
+
+  private animate() {
+    this.testCube.position.setZ(2 * Math.sin(this.t));
+    this.t += 0.03;
+  }
+
+  private startRenderingLoop() {
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas, antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    let component: AppComponent = this;
+    (function render() {
+      component.animate();
+      component.renderer.render(component.scene, component.camera);
+      requestAnimationFrame(render);
+    }());
+  }
 }
+
